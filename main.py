b88d54de4173de6fa3db146a17d3d0775b57be89
Blane Cummings <blanecummings@hotmail.com>
2026-01-01 15:25:11 -0600

    get user_prompt as input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 
 def main():
@@ -12,15 +13,17 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    parser = argparse.ArgumentParser(description="ai-agent")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=prompt,
+        contents=args.user_prompt,
     )
 
     if response.usage_metadata is not None:
-        print(f"User prompt: {prompt}")
+        print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         print(f"Response: {response.text}")
