45c55a59549948ad7fa7320535f0e2f2db1aff94
Blane Cummings <blanecummings@hotmail.com>
2026-01-01 15:36:09 -0600

    add verbose flage

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
 
     parser = argparse.ArgumentParser(description="ai-agent")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -26,9 +27,10 @@ def main():
     )
 
     if response.usage_metadata is not None:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         print(f"Response: {response.text}")
     else:
         raise RuntimeError("Response 'usage_metadata' is None.")
